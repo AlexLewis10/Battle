1d5d43febabcf3cc087b792292ca4a70780556b6
Alex L <macbookpro@macbooks-mbp.home>
2020-04-02 16:39:15 +0100

    test - attack instructs player to take damage - fail

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,7 +1,13 @@
 require 'game'
 
 describe Game do
+  let(:player) { double :player }
   it 'responds to attack' do
     expect(subject).to respond_to(:attack).with(1).argument
+  end
+
+  it "attack instructs the player to take damage" do
+    expect(player).to receive :take_damage
+    subject.attack(player)
   end
 end
